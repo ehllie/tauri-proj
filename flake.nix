7d84e2e6c4a32184a12fd563b120a6bb6b1c606f
Elizabeth Paź <me@ehllie.xyz>
2023-02-26 03:20:46 +0100

    Using cargo tauri

--- flake.nix
+++ flake.nix
@@ -6,9 +6,11 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
+      toml = builtins.fromTOML (builtins.readFile ./src-tauri/Cargo.toml);
       json = builtins.fromJSON (builtins.readFile "${self}/package.json");
 
-      name = json.name;
+      name = toml.package.name;
+      fe-name = json.name;
     in
     {
       overlays.default = final: prev:
@@ -18,6 +20,8 @@
             buildNpmPackage
             lib
             stdenv
+            cargo-tauri
+            writeShellScriptBin
             rustPlatform;
           inherit (darwin.apple_sdk.frameworks)
             Carbon
@@ -27,27 +31,30 @@
             AppKit;
 
           frontend = buildNpmPackage {
-            pname = name;
+            pname = fe-name;
             src = self;
             inherit (json) version;
             npmDepsHash = "sha256-me2Fsvafir/e/k8nbxDdy4lLuvnTT7m8k+GVs3p12MY=";
             postInstall = ''
-              cp -r .svelte-kit $out/lib/node_modules/${name}/build/
+              cp -r .svelte-kit $out/lib/node_modules/${fe-name}/build/
             '';
           };
 
-          toml = builtins.fromTOML (builtins.readFile ./src-tauri/Cargo.toml);
-
           tauri-pkg = rustPlatform.buildRustPackage {
-            pname = toml.package.name;
+            pname = name;
             src = ./src-tauri;
             inherit (toml.package) version;
             cargoLock.lockFile = ./src-tauri/Cargo.lock;
 
             postPatch = ''
               substituteInPlace tauri.conf.json --replace \
-                '"distDir": "../build"' '"distDir": "${frontend}/lib/node_modules/${name}/build"'
+                '"distDir": "../build"' '"distDir": "${frontend}/lib/node_modules/${fe-name}/build"'
             '';
+
+            nativeBuildInputs = [
+              cargo-tauri
+              (writeShellScriptBin "npm" "true")
+            ];
 
             buildInputs = lib.optionals stdenv.isDarwin [
               Carbon
@@ -56,23 +63,37 @@
               WebKit
               AppKit
             ];
+
+            buildPhase = ''
+              # function npm { true; }
+              # cargo tauri info
+              cargo tauri build -vb app
+            '';
+
+            installPhase = ''
+              mkdir -p $out/Applications
+              cp -r target/release/bundle/macos/*.app $out/Applications/
+            '';
+
+            passthru = rec {
+              shell = prev.mkShell {
+                name = "${fe-name}-devShell";
+                inputsFrom = [ tauri-pkg ];
+                packages = with prev; [
+                  cargo-tauri
+                  rust-analyzer
+                  clippy
+                  rustfmt
+                ];
+                shellHook = ''
+                  export PATH=$PWD/target/debug:$PATH
+                '';
+              };
+              cache = shell;
+            };
           };
         in
-        {
-          ${name} = tauri-pkg;
-          cache = prev.mkShell {
-            name = "${name}-devShell";
-            inputsFrom = [ tauri-pkg ];
-            packages = with prev; [
-              rust-analyzer
-              clippy
-              rustfmt
-            ];
-            shellHook = ''
-              export PATH=$PWD/target/debug:$PATH
-            '';
-          };
-        }
+        { ${name} = tauri-pkg; }
       ;
     } //
 
@@ -82,15 +103,14 @@
           inherit system;
           overlays = [ self.overlays.default ];
         };
-        inherit (pkgs) cache;
         package = pkgs.${name};
       in
       {
         packages = {
-          inherit cache;
           default = package;
           ${name} = package;
+          cache = package.passthru.cache;
         };
-        devShells.default = cache;
+        devShells.default = package.passthru.shell;
       }));
 }
